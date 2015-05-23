8524c6f3a5e1fac608b69d2af807954494e0ea9b
ewelinag <grudzien.e@gmail.com>
2015-05-23 19:07:24 +0200

    Fasada Boost

--- statki/src/fasadapy.cpp
+++ statki/src/fasadapy.cpp
@@ -15,6 +15,25 @@ using namespace boost::python;
 BOOST_PYTHON_MODULE( fasada )
 {
 	//! exports getNumber to Python
-	boost::python::def( "ConnectPlayer", ConnectPlayer );
-
+	//boost::python::def( "ConnectPlayer", ConnectPlayer );
+	
+	class_<MapPosition>("MapPosition", "TODO Description")
+				.def_readonly("x", &MapPosition::x)
+				.def_readonly("y", &MapPosition::y)
+				;	
+				
+	class_<ShotPosition>("ShotPosition", "TODO Description")
+				.def_readonly("x", &ShotPosition::x)
+				.def_readonly("y", &ShotPosition::y)
+				.def_readonly("TargetHit", &ShotPosition::TargetHit)
+				;	
+	
+	class_<GameData>("GameData", "TODO Description")
+				.def_readonly("ID", &GameData::ID)
+				.def_readonly("GameMode", &GameData::GameMode)
+				.def_readonly("MyShips", &GameData::MyShips)
+				.def_readonly("EnemyShots", &GameData::EnemyShots)
+				.def_readonly("MyShots", &GameData::MyShots)
+				;
+						
 }
